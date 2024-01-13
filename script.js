e522f910944de186ff1e8de000fa8514e0ab5ff7
teenit <teenitclub@gmail.com>
2024-01-13 02:43:12 +0200

    Added icons-font

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function showList(index) {
-    var data = [
-        ["Нейромедитація 1", "Практика для швидкої стабілізації емоційного стану"],
-        ["Нейромедитація 2", "Тест на обмеження", "Комплекс вправ на зняття спазмів"],
-        ["Біоенергетична практика 1", "Біоенергетична практика 2"],
-        ["Нейромедитація 3", "Практична робота: Вибори в щоденному житті через контакт з собою"],
-        ["Біоенергетична практика 3", "Нейромедитація 4", "Біоенергетична практика 4"],
-        ["Секретний подарунок"],
-    ];
+    // var data = [
+    //     ["Нейромедитація 1", "Практика для швидкої стабілізації емоційного стану"],
+    //     ["Нейромедитація 2", "Тест на обмеження", "Комплекс вправ на зняття спазмів"],
+    //     ["Біоенергетична практика 1", "Біоенергетична практика 2"],
+    //     ["Нейромедитація 3", "Практична робота: Вибори в щоденному житті через контакт з собою"],
+    //     ["Біоенергетична практика 3", "Нейромедитація 4", "Біоенергетична практика 4"],
+    //     ["Секретний подарунок"],
+    // ];
 
-    var desc = document.querySelectorAll(".program-body-blocks-block-description")[index]
-    var list = desc.querySelector("ul");
+     var desc = document.querySelectorAll(".program-body-blocks-block-description")[index]
+    // var list = desc.querySelector("ul");
 
-    if (!list) {
-        list = document.createElement("ul");
-        data[index].forEach(function (item) {
-            var li = document.createElement("li");
-            li.innerText = item;
-            list.appendChild(li);
-        });
-        desc.appendChild(list);
-    }
+    // if (!list) {
+    //     list = document.createElement("ul");
+    //     data[index].forEach(function (item) {
+    //         var li = document.createElement("li");
+    //         li.innerText = item;
+    //         list.appendChild(li);
+    //     });
+    //     desc.appendChild(list);
+    // }
 
     desc.classList.toggle("active");
-    list.style.display = desc.classList.contains("active") ? "block" : "none";
+   // list.style.display = desc.classList.contains("active") ? "block" : "none";
 }
